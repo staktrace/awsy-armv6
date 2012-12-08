9c3b907ddcf74c39f8e61f099194d45eebc1c5c7
Kartikaya Gupta <kgupta@mozilla.com>
2012-12-08 16:26:34 -0500

    Update rebuild script to generate flot data

--- graph-data-generator/rebuild-all.sh
+++ graph-data-generator/rebuild-all.sh
@@ -6,21 +6,33 @@ for i in ../data/*; do
         echo "Missing data in $i" > /dev/stderr
         continue
     fi
-    PID=${PID%%.*}
     PID=${PID##*-}
+    PID=${PID%%.*}
     TIMESTAMP=$(head -1 $i/fennec-*-armv6.txt)
     for j in Start StartSettled TabsOpen TabsOpenSettled TabsOpenForceGC TabsClosed TabsClosedSettled TabsClosedForceGC; do
-        zcat $i/memory-report-$j-$PID.jzon.gz | java -cp sts_util.jar com.staktrace.util.conv.json.Extractor -object - reports/path=resident/amount reports/path=explicit/amount |
-        while read resident explicit; do
-            echo "[ $TIMESTAMP, $resident ]" >> resident-$j.graphdata
-            echo "[ $TIMESTAMP, $explicit ]" >> explicit-$j.graphdata
+        zcat $i/memory-report-$j-$PID.json.gz | java -cp sts_util.jar com.staktrace.util.conv.json.Extractor -object - reports/path=resident/amount reports/path=explicit/amount |
+        while read resident; do
+            echo "      [ $TIMESTAMP, $resident ]," >> resident-$j.graphdata
+            read explicit;
+            echo "      [ $TIMESTAMP, $explicit ]," >> explicit-$j.graphdata
         done
     done
 done
 for i in resident explicit; do
     echo "[" >> $i.graphdata
     for j in Start StartSettled TabsOpen TabsOpenSettled TabsOpenForceGC TabsClosed TabsClosedSettled TabsClosedForceGC; do
+        echo "  {" >> $i.graphdata
+        echo '    "label": "' $j '",' >> $i.graphdata
+        echo '    "data": [' >> $i.graphdata
         cat $i-$j.graphdata >> $i.graphdata
+        rm $i-$j.graphdata
+        echo "      null" >> $i.graphdata
+        echo "    ]" >> $i.graphdata
+        if [ $j == "TabsClosedForceGC" ]; then
+            echo "  }" >> $i.graphdata
+        else
+            echo "  }," >> $i.graphdata
+        fi
     done
     echo "]" >> $i.graphdata
 done
