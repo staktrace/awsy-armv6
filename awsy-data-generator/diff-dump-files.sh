0dc16d646ff6f82f8b04b0b5dc6c672f5e9f2095
Kartikaya Gupta <kgupta@mozilla.com>
2013-02-01 14:16:03 -0500

    Roll the about-memory-differ into the awsy-data-generator folder

---
+++ awsy-data-generator/diff-dump-files.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+USAGE="$0 <before-dump-file> <after-dump-file>"
+BEFORE=${1?$USAGE}
+AFTER=${2?$USAGE}
+
+BEFORE_PLAIN=$(mktemp)
+AFTER_PLAIN=$(mktemp)
+(zcat $BEFORE 2>/dev/null || cat $BEFORE) > $BEFORE_PLAIN
+(zcat $AFTER 2>/dev/null || cat $AFTER) > $AFTER_PLAIN
+
+java -cp sts_util.jar:. Differ $BEFORE_PLAIN $AFTER_PLAIN
+
+rm $BEFORE_PLAIN
+rm $AFTER_PLAIN
