fe65bd69300c4a679ae999f477505501fe9a1903
Kartikaya Gupta <kgupta@mozilla.com>
2013-04-15 10:54:09 -0400

    Update pick-build.sh to not start re-running old builds in backwards order when it runs out of new ones

--- pick-build.sh
+++ pick-build.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
+STAGE=${STAGE-"http://stage.mozilla.org/pub/mozilla.org/mobile/tinderbox-builds/mozilla-inbound-android-armv6"}
+ROOT=${ROOT-"$HOME/awsy-armv6/data"}
+
 export MATCHED=0
 BUILDID=$(links -dump $STAGE/ | grep ' \[DIR\] ' | awk '{ print $2 }' | sed -e "s#/##" | sort -n |
     while read buildstamp; do
         if [[ -d $ROOT/$buildstamp ]]; then
             MATCHED=1
-        elif [[ $MATCHED -eq 1 ]]; then
-            echo $buildstamp
-            exit
+            echo ""
         else
             echo $buildstamp
+            if [[ $MATCHED -eq 1 ]]; then
+                exit
+            fi
         fi
     done |
     tail -n 1)
