aeb174b1312869118b8929b92b97cb78a5243257
Kartikaya Gupta <kgupta@mozilla.com>
2013-10-14 19:50:11 -0400

    Run fx-team ahead of b2g-inbound

--- run-next-integration-build.sh
+++ run-next-integration-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for tree in mozilla-inbound b2g-inbound fx-team; do
+for tree in mozilla-inbound fx-team b2g-inbound; do
     export STAGE="http://stage.mozilla.org/pub/mozilla.org/mobile/tinderbox-builds/$tree-android-armv6"
     export STAGE_POSTFIX=""
     export ROOT=$HOME/awsy-armv6/data/$tree
