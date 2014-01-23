3ef5a8e133bbfda77ab10355e5c9ae4bd28c449f
Kartikaya Gupta <kgupta@mozilla.com>
2014-01-23 11:52:32 -0500

    Update snappy script to run on inbound data

--- snappy-data-generator/generate-all.sh
+++ snappy-data-generator/generate-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in ../data/*; do
+for i in ../data/mozilla-inbound/*; do
     if [ -f $i/awsy.final.gz ]; then
         grep zerdatime $i/device.log | head -3 | awk -v build=$(tail -1 $i/fennec-*.txt) -f time.awk
     fi
